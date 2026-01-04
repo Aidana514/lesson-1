93c145cdb0a4defbc183828af2f7f866b3838e95
Aidana514 <karymshakova2024@gmail.com>
2026-01-04 22:12:12 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+const array = [
+    
+];
+
+const List = document.getElementById("List");
+const btn = document.getElementById("btn");
+const textInput = document.getElementById("text");
+const dateInput = document.getElementById("date");
+
+function getData(){
+    return array.forEach(
+        (item) => {
+            const li = document.createElement("li");
+            const textElement = document.createElement("p");
+            const dateElement = document.createElement("p");
+            const deleteBtn = document.createElement("button");
+            
+            deleteBtn.textContent = "Удалить";
+            deleteBtn.className = "delete";
+            textElement.textContent = item.text;
+            dateElement.textContent = item.date;
+            
+
+            li.append(textElement);
+            li.append(dateElement);
+            li.append(deleteBtn);
+            List.append(li)
+        }
+    );
+}
+
+
+function addData(){
+if(textInput.value.length > 0 && dateInput.value.length > 0){
+    array.push({
+        text:textInput.value,
+        date:dateInput.value,
+    })
+    List.innerHTML = "";
+    textInput.value = "";
+    dateInput.value = ""; 
+getData();
+} else{
+    alert("Заполните form");
+}
+}
+getData();
+
+btn.addEventListener("click", addData);
+
+
+
